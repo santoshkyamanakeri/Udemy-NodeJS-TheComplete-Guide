056054dc3a5c511f108ddba0dea14b8a53751dc4
santosh kyamanakeri <85493557+santoshkyamanakeri@users.noreply.github.com>
2023-07-12 22:08:54 +0530

    started basics of js and created a folder and created a file called play.js, and created variable var and added values of user

---
+++ BasicJavaScript/play.js
@@ -0,0 +1,8 @@
+var name = "Santosh";
+var age = 26;
+var hobbies = true;
+
+function summarizeUser(userName, userAge, userHobbies) {
+  return userName + " " + userAge + " " + userHobbies;
+}
+console.log(summarizeUser(name, age, hobbies));
